20cdae94b18b720c546f8cda2a47d73b7c5747b5
Sabr_System <ilyyasa03@gmail.com>
2024-03-18 09:04:19 +0700

    make users admin pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :admin, controller:{
+    sessions: 'admin/sessions',
+    registrations: 'admin/registrations'
+  }
+  devise_for :users, controller:{
+    sessions:  "user/sessions",
+  }
+  root 'pages#home'
+  get 'pages/users'
+  get 'pages/admin'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
